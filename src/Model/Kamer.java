7e9592f80fdb82b34a54a8223f14ff1efd3cec4e
Sander Groot Wesseldijk <heukertje@gmail.com>
2016-09-02 10:48:20 +0200

    Opmaak en huurder.html verbeterd

--- src/Model/Kamer.java
+++ src/Model/Kamer.java
@@ -4,7 +4,9 @@ package Model;
  *
  */
 public class Kamer {
+    private Verhuurder verhuurder;
     private double oppervlakte;
+    private int aantalPersonen;
     private double prijs;
     private int kamers;
 }
