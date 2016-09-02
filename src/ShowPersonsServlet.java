eb623323608ee1bc0c3346f6e0858bc7e4b23218
martenkrings <krings.marten@gmail.com>
2016-09-02 10:18:51 +0200

    login register fouteinlog rename showroomservlet

--- src/ShowPersonsServlet.java
+++ src/ShowPersonsServlet.java
@@ -6,10 +6,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Created by Sander on 1-9-2016.
+ * Created by Gebruiker on 9/2/2016.
  */
-@WebServlet("/ShowRoomsServlet")
-public class ShowRoomsServlet extends HttpServlet {
+@WebServlet("/ShowPersonsServlet")
+public class ShowPersonsServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
